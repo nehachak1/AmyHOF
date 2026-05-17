abac7b818836492fa923506f307ca2a0d6be3e82
Martin Zimmer <martin.zimmer@epfl.ch>
2026-05-18 01:11:16 +0200

    Correct interpreter test calls

--- test/scala/amyc/test/InterpreterTests.scala
+++ test/scala/amyc/test/InterpreterTests.scala
@@ -15,25 +15,21 @@ class InterpreterTests extends ExecutionTests {
     .andThen(TypeChecker)
     .andThen(Interpreter)
 
-
   /* Test to verify that the compose function works */
   @Test def testHOFCompose = shouldOutput(
-  inputFiles = List("HOFCompose"), 
-  outputFile = "HOFCompose", 
-  libraryFiles = List("Std", "Option", "List") 
+  List("Std", "Option", "List", "HOFCompose"),
+  "HOFCompose"
   )
 
   /* Test to verify that the map function works */
   @Test def testHOFMap = shouldOutput(
-  inputFiles = List("HOFMap"), 
-  outputFile = "HOFMap", 
-  libraryFiles = List("Std", "Option", "List") 
+    List("Std", "Option", "List", "HOFMap"),
+    "HOFMap"
   )
-  
+
   /* Test to verify that the FoldLeft function works */
   @Test def testHOFFold = shouldOutput(
-  inputFiles = List("HOFFold"), 
-  outputFile = "HOFFold", 
-  libraryFiles = List("Std", "Option", "List") 
+    List("Std", "Option", "List", "HOFFold"),
+    "HOFFold"
   )
 }
